ee5d87b7abb2a297215eb9cd7997d55be4a9108b
Giovanny Ospina <31867636+ospna@users.noreply.github.com>
2021-10-26 17:25:25 -0400

    test

--- AR/sketchContainer.js
+++ AR/sketchContainer.js
@@ -18,8 +18,8 @@ let parts;
 let leaf, flower;
 
 function preload(){
-  leaf = loadImage("images/leaf.svg");
-  flower = loadImage("images/flower.svg");
+  leaf = loadImage("images/AR Media/fused_leaves-01.svg");
+  flower = loadImage("images/AR Media/Radial/radial_pendulus_large-01.svg");
 }
 
 function setup() {
