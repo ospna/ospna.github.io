fa848800a327767fb49690ad6cb647f50d7d6a27
Giovanny Ospina <31867636+ospna@users.noreply.github.com>
2021-11-01 16:03:16 -0400

    test functions

--- AR/functions.js
+++ AR/functions.js
@@ -53,7 +53,7 @@ window.onload = function () {
         // or you can just open links, trigger actions...
         alert("Honeysuckle Color"); 
       })
-      document.querySelector(".radial-button").addEventListener("click", function () {
+      document.querySelector("#radial-button").addEventListener("click", function () {
         // here you can change also a-scene or a-entity properties, like
         // changing your 3D model source, size, position and so on
         // or you can just open links, trigger actions...
