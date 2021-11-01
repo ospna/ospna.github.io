39790b10adb717d57284f5087f33f157d4915820
Giovanny Ospina <31867636+ospna@users.noreply.github.com>
2021-11-01 13:58:24 -0400

    new buttons and image tests

--- AR/functions.js
+++ AR/functions.js
@@ -53,4 +53,22 @@ window.onload = function () {
         // or you can just open links, trigger actions...
         alert("Honeysuckle Color"); 
       })
+      document.querySelector(".radial-button").addEventListener("click", function () {
+        // here you can change also a-scene or a-entity properties, like
+        // changing your 3D model source, size, position and so on
+        // or you can just open links, trigger actions...
+        alert("You chose the Radial petal orientation"); 
+      })
+      document.querySelector(".two21-button").addEventListener("click", function () {
+        // here you can change also a-scene or a-entity properties, like
+        // changing your 3D model source, size, position and so on
+        // or you can just open links, trigger actions...
+        alert("You chose the 2+2+1 petal orientation"); 
+      })
+      document.querySelector(".four1-button").addEventListener("click", function () {
+        // here you can change also a-scene or a-entity properties, like
+        // changing your 3D model source, size, position and so on
+        // or you can just open links, trigger actions...
+        alert("You chose the 4+1 petal orientation"); 
+      })
   }
