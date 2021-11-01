84be2a725058c305e3b7f41fd39084a26e781b46
Giovanny Ospina <31867636+ospna@users.noreply.github.com>
2021-11-01 11:44:42 -0400

    separate and test

---
+++ AR/functions.js
@@ -0,0 +1,56 @@
+window.onload = function () {
+    document.querySelector("#small-scale-button").addEventListener("click", function () {
+        // here you can change also a-scene or a-entity properties, like
+        // changing your 3D model source, size, position and so on
+        // or you can just open links, trigger actions...
+        alert("Small Honeysuckle!"); 
+      })
+      document.querySelector("#medium-scale-button").addEventListener("click", function () {
+        // here you can change also a-scene or a-entity properties, like
+        // changing your 3D model source, size, position and so on
+        // or you can just open links, trigger actions...
+        alert("Medium Honeysuckle!");
+      })
+      document.querySelector("#large-scale-button").addEventListener("click", function () {
+        // here you can change also a-scene or a-entity properties, like
+        // changing your 3D model source, size, position and so on
+        // or you can just open links, trigger actions...
+        alert("Large Honeysuckle!");
+      });
+      document.querySelector(".size-button").addEventListener("click", function () {
+        // here you can change also a-scene or a-entity properties, like
+        // changing your 3D model source, size, position and so on
+        // or you can just open links, trigger actions...
+        alert("Change Size"); 
+      })
+      document.querySelector(".petal-button").addEventListener("click", function () {
+        // here you can change also a-scene or a-entity properties, like
+        // changing your 3D model source, size, position and so on
+        // or you can just open links, trigger actions...
+        alert("Petal Orientation"); 
+      })
+      document.querySelector(".flower-button").addEventListener("click", function () {
+        // here you can change also a-scene or a-entity properties, like
+        // changing your 3D model source, size, position and so on
+        // or you can just open links, trigger actions...
+        alert("Flower Orientation"); 
+      })
+      document.querySelector(".leaf-button").addEventListener("click", function () {
+        // here you can change also a-scene or a-entity properties, like
+        // changing your 3D model source, size, position and so on
+        // or you can just open links, trigger actions...
+        alert("Leaf Fusion"); 
+      })
+      document.querySelector(".growth-button").addEventListener("click", function () {
+        // here you can change also a-scene or a-entity properties, like
+        // changing your 3D model source, size, position and so on
+        // or you can just open links, trigger actions...
+        alert("Growth Habit"); 
+      })
+      document.querySelector(".color-button").addEventListener("click", function () {
+        // here you can change also a-scene or a-entity properties, like
+        // changing your 3D model source, size, position and so on
+        // or you can just open links, trigger actions...
+        alert("Honeysuckle Color"); 
+      })
+  }
